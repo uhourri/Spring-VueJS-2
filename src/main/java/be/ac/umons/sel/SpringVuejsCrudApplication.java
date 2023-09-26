f9480c167dbbbd50570bd4433fcd15383b5697b3
uhourri <hourri@yahoo.com>
2023-09-26 15:17:50 +0200

    Commit 21

---
+++ src/main/java/be/ac/umons/sel/SpringVuejsCrudApplication.java
@@ -0,0 +1,13 @@
+package be.ac.umons.sel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVuejsCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVuejsCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ac/umons/sel/SpringVuejsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ac.umons.sel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVuejsCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
